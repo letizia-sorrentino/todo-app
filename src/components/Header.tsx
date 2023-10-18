7fe8343d7d705124ba8dfd649c5731e080a2b14d
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-10-18 10:47:26 +0100

    backup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import mobileLight from "../assets/bg-mobile-light.jpg";
+import iconMoon from "../assets/icon-moon.svg";
 
 const Header = () => {
   return (
     <>
+      <img className="headerImage" src={mobileLight} alt="mobile-light" />  
+      <div className="headerContainer">
       <h1 className="appTitle">TO DO</h1>
-      <img className="headerImage" src={mobileLight} alt="mobileLight" />
+      <img className="iconMoon" src={iconMoon} alt="icon-moon" />
+      </div>
     </>
   );
 };
