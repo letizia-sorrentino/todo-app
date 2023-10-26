59462cc9aec8f6d386887eeef0995840e0656b8c
Letizia <116899981+letizia-sorrentino@users.noreply.github.com>
2023-10-26 11:50:06 +0200

    restyled app

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,12 +2,14 @@ import "../styles/Footer.css";
 
 const Footer = () => {
   return (
-    <div className="attribution">
-      Challenge by{" "}
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-        Frontend Mentor
-      </a>
-      . Coded by <a href="#">Letizia S</a>.
+    <div className="AppFooter">
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Letizia S</a>.
+      </div>
     </div>
   );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
-import mobileLight from "../assets/bg-mobile-light.jpg";
+import mobileBackground from "../assets/bg-mobile-light.jpg";
 import iconMoon from "../assets/icon-moon.svg";
 import "../styles/Header.css";
 
 const Header = () => {
   return (
     <>
-      <img className="headerImage" src={mobileLight} alt="mobile-light" />
+      <img className="headerImage" src={mobileBackground} alt="mobile-light" />
       <div className="headerContainer">
-        <h1 className="appTitle">TO DO</h1>
+        <h1 className="appTitle">TODO</h1>
         <img className="iconMoon" src={iconMoon} alt="icon-moon" />
       </div>
     </>
